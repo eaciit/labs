98a14b2479d449505469e741a3e1e73d0d06ace0
Arief Darmawan <ariefda@hotmail.com>
2016-01-14 00:23:19 +0700

    slice

--- reflect.go
+++ reflect.go
@@ -14,31 +14,32 @@ type Employee struct {
 
 func main() {
 	//emp := new(Employee)
-	emps := []Employee{}
+	emps := tk.MakeSlice(&Employee{}).([]*Employee)
 	fillArray(&emps)
 	fmt.Printf("Value of empls:\n%s\n", tk.JsonString(emps))
 }
 
 func fillArray(o interface{}) {
-	//rt := reflect.TypeOf(o)
-	isPointer := false
 	rv1 := reflect.ValueOf(o)
 	var rv2 reflect.Value
 
 	if rv1.Kind() == reflect.Ptr {
-		isPointer = true
 		rv2 = reflect.Indirect(rv1)
 	} else {
-		rv2 = rv1
+		fmt.Println("Object passed is not a ptr")
+		return
 	}
 
 	if rv2.Kind() == reflect.Slice {
-		//fmt.Println("Add new element")
-		rv2 = reflect.Append(rv2, reflect.ValueOf(Employee{"EC0003", "Arief Darmawan", 30}))
-		if isPointer {
-			rv1.Elem().Set(rv2)
-
+		tslice := rv2.Type().Elem()
+		var newEmp reflect.Value
+		if string(tslice.String()[0]) == "*" {
+			newEmp = reflect.New(tslice.Elem())
+		} else {
+			newEmp = reflect.Indirect(reflect.New(tslice.Elem()))
 		}
+		rv2 = reflect.Append(rv2, newEmp)
+		rv1.Elem().Set(rv2)
 	} else {
 		fmt.Printf("Kind: %s\n", rv2.Kind().String())
 	}
